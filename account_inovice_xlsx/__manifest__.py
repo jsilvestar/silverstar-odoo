a3163079fe167a7b719556d928de6e955f50b98f
jsilvestar <jsilverstar19@gmail.com>
2019-05-03 16:31:39 +0530

    [ADD] Added account invoice xlsx.

---
+++ account_inovice_xlsx/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Invoice Xlsx',
+    'version': '1.0',
+    'category': 'Accounting',
+    "description": """
+        Account Invoice Xlsx.
+    """,
+    'author': 'Silverstar',
+    'depends': ['account'],
+    'data': [
+        'wizard/account_invoice_view.xml'
+        ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
